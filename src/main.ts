3c0e7dd640b41ca0ae2f527cb33d25e80391d269
Jhoandris B <jrbaronp@gmail.com>
2024-08-13 10:07:28 -0500

    dockler

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+  //Logger
+  app.use(morgan('dev'));
+  //ConfigService
+  const configService = app.get(ConfigService);
+  //PORT
+  const port = configService.get('PORT');
+
+  //Global Prefix
+  app.setGlobalPrefix('api');
+  //Enable Cors
+  app.enableCors(CORS);
+  //Start Server
+  await app.listen(port);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
