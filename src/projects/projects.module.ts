b6020fe8f77ae8806a7dc46f62624c4553066618
Jhoandris B <jrbaronp@gmail.com>
2024-08-15 09:28:37 -0500

    project services

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProjectsService } from './services/projects.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsEntity } from './entities/projects.entity';
+import { ProjectsController } from './controllers/projects.controller';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ProjectsEntity])],
+  providers: [ProjectsService],
+  controllers: [ProjectsController],
+})
 export class ProjectsModule {}
